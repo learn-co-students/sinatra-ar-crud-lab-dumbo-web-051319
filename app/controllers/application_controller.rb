5c98695200178ffc16c8dc065f67d5603960159d
Your Name <you@example.com>
2019-06-07 11:31:53 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,47 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
-
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles' do
+    @articles = Article.all
+      erb :index
+  end
+
+  get '/articles/new' do
+     erb :new
+  end
+
+  post '/articles' do
+    Article.create(params)
+    last_id = Article.all.last.id
+    # binding.pry
+    redirect "/articles/#{last_id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id])
+    erb :edit
+  end
+
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    erb :show
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    @article.update(params["article"])
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    # binding.pry
+    @article.destroy
+    redirect "/"
   end
 end
